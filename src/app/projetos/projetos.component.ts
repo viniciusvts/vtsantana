44ec77e010bc60050e83a6c8855b688f6e49a13a
Vinicius de Santana <vinicius@vts>
2017-12-28 21:03:02 -0300

    Criação e configuração da página projetos

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Projeto } from '../../assets/Projeto';
+import { AppService } from '../app.service';
+
 @Component({
   selector: 'app-projetos',
   templateUrl: './projetos.component.html',
   styleUrls: ['./projetos.component.css']
 })
 export class ProjetosComponent implements OnInit {
+  projs: Array<Projeto>;
 
-  constructor() { }
+  constructor(private _projQry: AppService) {
+    this.ObtemListaProj();
+  }
+
+  ObtemListaProj(): void {
+    this._projQry.getProjects()
+      .subscribe((data: Projeto[]) => this.projs = data,
+      error => console.log(error));
+  }
 
   ngOnInit() {
   }
