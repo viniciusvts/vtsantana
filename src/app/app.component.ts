82dc8e8ae3d59bb8866d30c473b86f3c88b80d0b
Vinicius de Santana <vinicius.vts@gmail.com>
2018-10-19 22:32:05 -0300

    A lógica não é mais feita na view e sim no controle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  guia = 'home';
+  guia: number;
+  pagina = NumeroGuia; // Numero de guia é o enum criado para passar as páginas
+  // gets e sets de guia
+  setGuia(num: number): void {
+    this.guia = num;
+  }
+  isGuia( num: number): boolean {
+    if ( this.guia === num) {
+      return true;
+    }
+    return false;
+  }
+  // gets e set de guia
+  constructor() {
+    this.setGuia(this.pagina.Home);
+  }
 
+}
+
+export enum NumeroGuia {
+    Home = 0,
+    Proj = 1
 }
